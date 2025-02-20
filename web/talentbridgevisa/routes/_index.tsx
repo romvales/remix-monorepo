d8ce47dfba99d15eac514797de3ee2ac9dec376a
Rom Vales Villanueva <romdevmod@gmail.com>
2025-02-20 10:47:10 +0800

    Minimal Remix setup

--- web/talentbridgevisa/routes/_index.tsx
+++ web/talentbridgevisa/routes/_index.tsx
@@ -1,5 +1,10 @@
 import { Button } from '@components/ui/button';
+import { LoaderFunctionArgs } from '@remix-run/node';
 
+
+export const loader = async (args: LoaderFunctionArgs) => {
+  
+}
 
 export default function Home() {
   return (
